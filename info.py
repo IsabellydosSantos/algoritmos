2b9eee532850bf508e3603cfef5242e80840f5d5
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-08-19 10:56:23 -0400

    Delete info.py

--- info.py
+++
@@ -1,6 +0,0 @@
-nome = str(input("Informe seu nome: "))
-ra = int(input("Informe seu RA: "))
-coddisc = int(input("Informe o código da sua disciplina: "))
-ano = int(input("Informe o ano: "))
-
-print(nome, ra, coddisc, ano, sep=" - ")
