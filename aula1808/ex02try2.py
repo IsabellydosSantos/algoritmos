98d479f1ac934e8f19e0d536105f2be77262b38b
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-08-19 14:23:49 -0400

    ex02try2.py

--- aula1808/ex02try2.py
+++ aula1808/ex02try2.py
@@ -1,5 +1,5 @@
 hora_inicial = 14  
 horas_adicionais = 51
-hora_final = (hora_inicial + horas_adicionais) % 24
+hora_alarme = (hora_inicial + horas_adicionais) % 24
 
-print(f"O alarme irá tocar às {hora_final}:00 horas")
+print(f"O alarme irá tocar às {hora_alarme}:00 horas")
