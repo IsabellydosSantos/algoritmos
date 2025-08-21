5016f94f046ba2ad28d750209f721915e4d60945
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-08-21 19:47:23 -0400

    ex05.py

--- lista1exercicios/ex05.py
+++ lista1exercicios/ex05.py
@@ -4,8 +4,8 @@ temp = float(input("Insira a temperatura: "))
 if unitemp == "F":
     convC = (5/9) * (temp-32)
     roundedC = round(convC, 2)
-    print("A temperatura {} F em Celsius é {} C".format(temp, convC))
+    print("A temperatura {} F em Celsius é {} C".format(temp, roundedC))
 elif unitemp == "C":
     convF = (temp * 9/5) + 32
     roundedF = round(convF,2)
-    print("A temperatura {} C em Fahrenheit é {} F".format(temp, convF))
+    print("A temperatura {} C em Fahrenheit é {} F".format(temp, roundedF))
