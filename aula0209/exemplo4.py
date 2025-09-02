98df305746dcd2710825c29698483052706a66c5
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-02 10:54:00 -0400

    Add files via upload

---
+++ aula0209/exemplo4.py
@@ -0,0 +1,8 @@
+base = int(input("Base: "))
+exp = int(input("Expoente: "))
+result = base
+i = 1
+while i < exp:
+  result = result * base
+  i = i + 1
+print(result)
---
+++ aula0209/exemplo5.py
@@ -0,0 +1,6 @@
+n = int(input("Entre com um número inteiro positivo: "))
+fat = 1
+while n > 1:
+    fat = fat * n
+    n = n - 1
+print("Resultado:", fat)
