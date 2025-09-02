fb1ea779597fd740b4fd4f68b23cd2d197ff4249
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-02 09:41:23 -0400

    ex08.py

--- lista1exercicios/ex08.py
+++ lista1exercicios/ex08.py
@@ -1,10 +1,10 @@
 sal = float(input("Insira o valor de seu salário (R$) : "))
 
 if sal < 1000.00:
-  print("Não há impostos a pagar")
-elif sal >= 1000.00 or sal <= 3000.00:
-  calc = sal * 0.2 
-  print("Você deve pagar R${} reais de imposto.").format(calc)
+    print("Não há impostos a pagar")
+elif 1000.00 <= sal <= 3000.00:
+    calc = sal * 0.2
+    print(f"Você deve pagar R${calc} reais de imposto.")
 else:
-  calc = sal * 0.35
-  print("Você deve pagar R${} reais de imposto.").format(calc)
+    calcm = sal * 0.35
+    print(f"Você deve pagar R${calcm} reais de imposto.")
