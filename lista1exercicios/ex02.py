7a302d3e7a2fabe63f59ea999966756b8916ed21
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 11:09:58 -0400

    ex02.py

--- lista1exercicios/ex02.py
+++ lista1exercicios/ex02.py
@@ -1,8 +1,13 @@
+#lê dois valores inteiros x e y e troca os valores
 x = int(input("Digite o valor de x: "))
 y = int(input("Digite o valor de y: "))
 
 print(f"Valores originais: x = {x}, y = {y}")
 
+#troca os valores
+#exemplo: x=2 e y=10
+#depois da troca: x=10 e y=2
 x, y = y, x
 
 print(f"Valores trocados: x = {x}, y = {y}")
+
