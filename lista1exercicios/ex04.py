534e89bc4aaa54bd779aec2979683737015b4d26
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 11:21:35 -0400

    ex04.py

--- lista1exercicios/ex04.py
+++ lista1exercicios/ex04.py
@@ -1,11 +1,14 @@
-print("Digite um número: ")
-a = int(input())
-if a % 2 == 0 and a < 100:
-    print("O número é par e menor que 100")
-elif a % 2 == 0 and a >= 100:
-        print("O número é par e maior ou igual a 100")
+#classifica o número inserido como par ou ímpar e se é menor, maior ou igual a 100
 
-if a % 2 != 0 and a < 100:
+a = int(input("Insira um número: "))
+#"a % 2 == 0" verifica se o número é par
+#para um número ser par é necessário que a divisão do número por 2 tenha resto igual a 0
+if a % 2 == 0 and a < 100: #se o numero é par e  menor que 100
+    print("O número é par e menor que 100")
+elif a % 2 == 0 and a >= 100:  #se o numero é par e menor ou igual a 100
+    print("O número é par e maior ou igual a 100")
+
+if a % 2 != 0 and a < 100: #se o número é ímpar e menor que 100
     print("O número é ímpar e menor que 100")
-elif a % 2 != 0 and a > 100:
-        print("O número é ímpar e maior que 100")
+elif a % 2 != 0 and a > 100: #se o número é ímpar e maior que 100
+    print("O número é ímpar e maior que 100")
