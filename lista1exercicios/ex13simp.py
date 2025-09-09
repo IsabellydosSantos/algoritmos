70f24fc081f8b9c621ef2c807cb3141bdc2b668d
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 15:27:10 -0400

    ex13simp.py

--- lista1exercicios/ex13simp.py
+++ lista1exercicios/ex13simp.py
@@ -1,4 +1,6 @@
 #recebe um número e o converte em sua base binária com uma função simples
 
 numero = int(input("Digite um número decimal: "))
+#em python o resultado é mostrado com um "0b" antes para indicar que está em base binária 
+#a função "[2: ]" indica que é para excluir esses dois primeiros dígitos
 print(f"O número {numero} em binário é: {bin(numero)[2:]}")
