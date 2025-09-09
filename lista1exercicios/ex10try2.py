c8959da9e327fc9320aef73d9c7d740f3de2cedf
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 14:53:07 -0400

    ex10try2.py

--- lista1exercicios/ex10try2.py
+++ lista1exercicios/ex10try2.py
@@ -1,23 +1,26 @@
+#lê a sequência com quantos números o usuário inserir, sem limite, e informa se está em ordem crescente ou não 
 entrada = input("Digite números separados por vírgula: ")
 
-# Divide a string de entrada em uma lista de strings usando vírgula como separador
+#divide a string de entrada em uma lista de strings usando vírgula como separador
 numeros_str = entrada.split(',')
 
-# Converte cada string da lista em número decimal (float)
+#converte cada string da lista em número decimal (float)
 numeros = [float(num) for num in numeros_str]
 
-# Variável que indica se a sequência está ordenada (inicialmente assumimos que está)
+#variável que indica se a sequência está ordenada (inicialmente assumimos que está)
 ordenada = True
 
-# Percorre a lista comparando cada número com o próximo
-# range(len(numeros) - 1) garante que não haverão mais comparações que necessário
+#percorre a lista comparando cada número com o próximo
+#range(len(numeros) - 1) garante que não haverão mais comparações que necessário
+#len indica a quantidade de números na lista (numeros)
 for i in range(len(numeros) - 1):
-    # Se encontrar um número maior que o próximo, a sequência não está ordenada
+    #se encontrar um número maior que o próximo, a sequência não está ordenada
     if numeros[i] > numeros[i + 1]:
         ordenada = False
-        break  # Interrompe o loop antecipadamente
+        break  #imterrompe o loop antecipadamente
 
 if ordenada:
     print("A sequência está ordenada de forma crescente.")
 else:
     print("A sequência não está ordenada de forma crescente.")
+
