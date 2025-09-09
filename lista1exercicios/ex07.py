d7bd2d4fb8a18df67c4591a17ba2b01ca93b1b3d
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 14:07:16 -0400

    ex07.py

--- lista1exercicios/ex07.py
+++ lista1exercicios/ex07.py
@@ -1,17 +1,20 @@
-sexo = str(input("Insira seu sexo (F ou M): ")).upper()
+#indica se a pessoa pode se aposentar ou não 
+
+sexo = str(input("Insira seu sexo (F ou M): ")).upper() #a função upper() coloca a reposta do usuário em maiúsculo
 idade = int(input("Insira sua idade: "))
 tempo_contrib = int(input("Insira seus anos de contribuição: "))
 
-if sexo == "M":
+if sexo == "M": #se a pessoa for do sexo masculino 
     if idade >= 65 and tempo_contrib >= 10:
         print("Aposentável")
     elif idade >= 63 and tempo_contrib >= 15:
         print("Aposentável")
 
-elif sexo == "F":
+elif sexo == "F": #se a pessoa for do sexo feminino
     if idade >= 63 and tempo_contrib >= 10:
         print("Aposentável")
     elif idade >= 61 and tempo_contrib >= 15:
         print("Aposentável")
 else:
     print("Não Aposentável")
+
