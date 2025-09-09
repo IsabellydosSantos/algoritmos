02efd00ed1c38038d79674251076cc8c2d0f88c7
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 15:10:23 -0400

    ex11try2.py

--- lista1exercicios/ex11try2.py
+++ lista1exercicios/ex11try2.py
@@ -2,11 +2,12 @@
 
 n = int(input("Digite um número inteiro positivo: "))
 
+#mostra até qual número será a busca por ímpares
+print(f"Números ímpares até {n}:")
+
 #loop que percorre os números de 1 até n, pulando de 2 em 2
 #range(início, fim, passo) → gera: 1, 3, 5, 7, ... até n ou o último ímpar antes de n
 for i in range(1, n + 1, 2):
-    #mostra até qual número será a busca por ímpares
-    print(f"Números ímpares até {n}:")
     #imprime cada número ímpar na mesma linha, separado por espaço
     #end=" " → substitui a quebra de linha padrão por espaço
     print(i, end=" ")
