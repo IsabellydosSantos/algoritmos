dee864438d7780c55df5c031fdcd045373c26f6d
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 14:04:05 -0400

    ex06.py

--- lista1exercicios/ex06.py
+++ lista1exercicios/ex06.py
@@ -1,7 +1,11 @@
+#lê um ano e informa se é bissexto ou não 
 ano = int(input("Insira um ano para saber se ele é bissexto: "))
 
 if (ano % 400 == 0) or (ano % 4 == 0 and ano % 100 != 0): 
+#um ano é bissexto se for divisível por 400 ou se for divisível por 4 mas não por 100
+#para um número ser divisível por outro, o resto da divisão deve ser 0
   print("Esse ano é bissexto")
 else:
   print("Esse ano não é bissexto")
+
 
