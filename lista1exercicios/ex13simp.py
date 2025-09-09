35f1caa250fb3d8b8f2b3b920d5f52320e8a0f33
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-09 15:21:17 -0400

    ex13simp.py

--- lista1exercicios/ex13simp.py
+++ lista1exercicios/ex13simp.py
@@ -1,2 +1,4 @@
+#recebe um número e o converte em sua base binária com uma função simples
+
 numero = int(input("Digite um número decimal: "))
 print(f"O número {numero} em binário é: {bin(numero)[2:]}")
