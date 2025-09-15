fe9e66e7ee5ee639a1de0ea9c39a83c60a9c7ac5
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-14 21:54:42 -0400

    ex17.py

--- aula1109/ex17.py
+++ aula1109/ex17.py
@@ -3,9 +3,23 @@ alunos = int(input("Insira a quantidade de alunos na sala: "))
 p1 = []
 p2 = []
 
+print("Insira as notas da P1: ")
 for i in range(alunos):
-  p1.append(int(input(f"Insira as notas da P1: ")))
+  p1.append(float(input()))
 
+print("Insira as notas da P2: ")
 for i in range(alunos):
-  p2.append(int(input(f"Insira as notas da P2: ")))
+  p2.append(float(input()))
 
+media_p1 = sum(p1) / alunos
+media_p2 = sum(p2) / alunos
+
+print(f"Média da turma na Prova 1: {media_p1:.2f}")
+print(f"Média da turma na Prova 2: {media_p2:.2f}")
+
+if media_p1 > media_p2:
+    print("A turma obteve a melhor média na Prova 1")
+elif media_p2 > media_p1:
+    print("A turma obteve a melhor média na Prova 2")
+else:
+    print("As duas provas tiveram a mesma média")
