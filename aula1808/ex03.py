c84ea06ab9c6824180f00a40aa1177d6a11b90c8
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-22 21:50:09 -0400

    ex03.py

--- aula1808/ex03.py
+++ aula1808/ex03.py
@@ -3,10 +3,9 @@ n2 = int(input("Informe o segundo valor inteiro: "))
 
 if n1 <= 0 or n2 <= 0:
     print("Inválido")
-elif n1 != int(n1) or n2 != int(n2):
-    print("Inválido")
 else:  
     if (n1 % 2) == (n2 % 2):
      print("False")  # Mesma paridade
     else:
      print("True")   # Paridades diferentes
+
