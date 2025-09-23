f248558192082fc5957e6fed50149054a6779bda
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-22 20:31:20 -0400

    ex09.py

--- aula0409/ex09.py
+++ aula0409/ex09.py
@@ -1,3 +1,5 @@
+#imprime os divisores de n
+
 n = int(input("Insira um número inteiro positivo (n>1): "))
 
 divisores = []
@@ -8,3 +10,4 @@ for i in range(1, n + 1):
     elif n % i == 0:
         divisores.append(i)
         print(f"Os divisores de {n} são {divisores}")
+
