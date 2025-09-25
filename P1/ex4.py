7b6bf72293754c08eb269c14f93546e6ba22fd67
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-09-25 10:18:34 -0400

    ex4.py

--- P1/ex4.py
+++ P1/ex4.py
@@ -2,7 +2,7 @@ x = int(input("Insira um número  inteiro positivo: "))
 y = int(input("Insira um número  inteiro positivo: "))
 z = int(input("Insira um número  inteiro positivo: "))
 
-if (x+y > z) and (x+z > y) and (z+y > x):
+if (x + y > z) and (x + z > y) and (z + y > x):
     if x==y==z:
         print("O triângulo é válido e equilátero")
     elif x!=y!=z:
@@ -10,4 +10,5 @@ if (x+y > z) and (x+z > y) and (z+y > x):
     else:
         print("O triângulo é válido e isósceles")
 else:
+
     print("Os valores não podem formar um triângulo, logo ele não é válido")
