ba1933d9eeb9871192ffb6e7f9f5617a0f0e918a
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-10-04 17:28:14 -0400

    ex30.py

--- aula0210/ex30.py
+++ aula0210/ex30.py
@@ -35,7 +35,6 @@ mais = max(contagem.values()) # Face que mais apareceu
 menos = min(contagem.values()) # Face que menos apareceu
 diferenca = mais - menos # Calcula o desequilíbrio 
 
-print(f"\nAnálise:")
 print(f"Diferença máxima: {diferenca}")
 
 if diferenca > freq_esperada * 0.7:
@@ -43,6 +42,7 @@ if diferenca > freq_esperada * 0.7:
   print("⚠️  Possível dado viciado")
 else:
     print("✓  Dado parece honesto")
+
 
 
 
