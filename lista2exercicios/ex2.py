b592560e139e4948ce10096d67c00ea329feea93
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-10-22 18:34:21 -0400

    Update ex2.py

--- lista2exercicios/ex2.py
+++ lista2exercicios/ex2.py
@@ -1,8 +1,8 @@
 c1 = input("\nDigite os números do primeiro conjunto (separados por espaço):")
-conjunto1 = [for x in c1.split()]
+conjunto1 = [int(x) for x in c1.split()]
 
 c2 = input("Digite os números do segundo conjunto (separados por espaço):")
-conjunto2 = [for x in c2.split()]
+conjunto2 = [int(x) for x in c2.split()]
     
 conjunto3 = conjunto1 + conjunto2
     
@@ -15,3 +15,4 @@ uniao.sort()
 # Exibe os resultados
 print(f"União dos conjuntos: {uniao}")
       
+
