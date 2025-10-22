1cdc028230a2be3df2dc43d845d4fc0136f78026
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-10-22 19:30:38 -0400

    Refactor input method to remove str() conversion

--- lista2exercicios/ex5.py
+++ lista2exercicios/ex5.py
@@ -1,5 +1,5 @@
-primeira = str(input("\nDigite a primeira string (caracteres a eliminar): "))
-segunda = str(input("Digite a segunda string (texto original): "))
+primeira = input("\nDigite a primeira string (caracteres a eliminar): ")
+segunda = input("Digite a segunda string (texto original): ")
     
 resultado = segunda
 for char in primeira: 
