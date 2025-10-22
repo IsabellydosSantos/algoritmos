728d654a8c040312480b7cca8974a4685b0e30fb
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-10-22 19:06:02 -0400

    Update ex3.py

--- lista2exercicios/ex3.py
+++ lista2exercicios/ex3.py
@@ -2,18 +2,18 @@ n = input("\nDigite 10 números inteiros separados por espaço:")
 vetor = [int(x) for x in n.split()]
     
 if len(vetor) != 10:
-    
-    # Calcula a maior diferença
+    print("Digite apenas 10 números")
+else:
     maior_diferenca = 0
     posicao_maior = 0
     
-    for i in range(9):  # 9 comparações para 10 elementos
+    for i in range(len(vetor) - 1):  
+        # abs(valor absoluto) garante que a diferença seja sempre positiva
+        # vetor[i] e vetor[i+1] garante que sejam consecutivos
         diferenca = abs(vetor[i] - vetor[i + 1])
         if diferenca > maior_diferenca:
             maior_diferenca = diferenca
             posicao_maior = i
     
-    # Exibe o resultado
-    print(f"\nVetor: {vetor}")
     print(f"Maior diferença: {maior_diferenca}")
     print(f"Entre vetor[{posicao_maior}] = {vetor[posicao_maior]} e vetor[{posicao_maior + 1}] = {vetor[posicao_maior + 1]}")
