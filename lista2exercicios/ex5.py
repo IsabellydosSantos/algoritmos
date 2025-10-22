5d6739d126abbc05b612aad4a970fb691945308d
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-10-22 19:32:07 -0400

    Delete lista2exercicios/ex5.py

--- lista2exercicios/ex5.py
+++
@@ -1,8 +0,0 @@
-primeira = input("\nDigite a primeira string (caracteres a eliminar): ")
-segunda = input("Digite a segunda string (texto original): ")
-    
-resultado = segunda
-for char in primeira: 
-  resultado = resultado.replace(char, '')
-    
-print(f"String resultante: '{resultado}'")
