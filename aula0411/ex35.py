6d434e992c75e127e772254832af9939ed43c3a2
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-11-04 10:34:28 -0400

    Add files via upload

---
+++ aula0411/ex35.py
@@ -0,0 +1,30 @@
+def mdc(a, b):
+    # Troca automática se necessário (sempre a >= b)
+    if a < b:
+        a, b = b, a  # Troca os valores
+
+    while b != 0:
+        resto = a % b
+        a, b = b, resto
+
+        return a
+
+def main():
+    try:
+        n1 = int(input("Insira o primeiro número inteiro positivo: "))
+        n2 = int(input("Insira o segundoo número inteiro positivo: "))
+
+        if n1 < 0 or n2 < 0:
+            print("Digite apenas números positivos")
+            return
+
+        resultado = mdc(n1, n2)
+
+        print(f"MDC({n1},{n2})")
+        print(resultado)
+
+    except ValueError:
+        print("Digite apenas números inteiros")
+
+
+main()
