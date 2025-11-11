21aee560e63321ae999de7991092b61dfce9460f
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-11-11 08:20:11 -0400

    Update ex50.py

--- revisaop2/ex50.py
+++ revisaop2/ex50.py
@@ -1,16 +1,15 @@
 def num_tri(n):
-
     if n < 0:
-        return False
+        return False, None
     elif n == 0:
-        return True
+        return True, (0, 1, 2)
 
     i = 0
     while i * (i+1) * (i+2) <= n:
         if i * (i+1) * (i+2) == n:
-            return True
+            return True, (i, i+1, i+2)
         i += 1
-    return False
+    return False, None
 
 
 def main():
@@ -19,17 +18,19 @@ def main():
 
         if n < 0:
             print("Digite apenas números positivos")
+            return
 
-        resultado = num_tri(n)
+        resultado, numeros = num_tri(n)
 
+        print(f"Retorno da função: {resultado}")
+        
         if resultado:
-            print(f"O número {n} é triangular e é formado por {i} * {i+1} * {i+2}")
+            print(f"O número {n} é triangular e é formado por {numeros[0]} * {numeros[1]} * {numeros[2]}")
         else:
             print(f"O número {n} não é triangular")
 
     except ValueError:
         print("Digite apenas números inteiros")
-
 
 
 main()
