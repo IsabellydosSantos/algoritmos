9b48a0635f1b097401e141971ea89cb739befe3c
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-12-02 10:41:56 -0400

    Create ex1.py

---
+++ P2/ex1.py
@@ -0,0 +1,12 @@
+texto = input("Insira um texto sem acentos: ").lower().strip()
+
+vogais = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+espacos = 0
+
+for caractere in texto:
+    if caractere == ' ':
+        espacos += 1
+    elif caractere in vogais:
+        vogais[caractere] += 1
+
+print(f"Espaços: {espacos}, a: {vogais['a']}, e: {vogais['e']}, i: {vogais['i']}, o: {vogais['o']}, u: {vogais['u']}")
