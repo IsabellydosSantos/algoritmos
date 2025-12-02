650320cb01a2c81ad1ffd5a267e4d4f67ec1b965
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2025-12-02 10:44:12 -0400

    Create ex4.py

---
+++ P2/ex4.py
@@ -0,0 +1,23 @@
+def num_tri(n):
+    if n < 0:
+        return False, None
+    elif n == 0:
+        return True, (0,1,2)
+
+    i = 0
+    while i*(i+1)*(i+2) <= n:
+        if i*(i+1)*(i+2) == n:
+            return True, (i, i+1, i+2)
+        i += 1
+    return False, None
+
+
+def main():
+    n = int(input("Insira um número inteiro positivo: "))
+
+    resultado, elementos = num_tri(n)
+
+    print(resultado, elementos)
+
+
+main()
